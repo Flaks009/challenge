40b8d9bbfd40174b1343167522c81d4897a6b1a6
flaks009 <flaks009@gmail.com>
2019-08-23 12:37:51 -0300

    rubocop

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,55 +1,54 @@
+# frozen_string_literal: true
+
 class ProfilesController < ApplicationController
-  before_action :set_profile, only: [:show, :destroy]
+  before_action :set_profile, only: %i[show destroy]
   def scrapping
-      @name = params[:name]
-      if @name
-          require 'open-uri'
-          require 'shorturl'
-          @doc = Nokogiri::HTML(open('https://twitter.com/' + @name))
-          @name = @doc.css('.ProfileHeaderCard-nameLink').text
-          @bio = @doc.css('.ProfileHeaderCard-bio').text
-          @link = 'https://twitter.com/' + @name
-          @link = ShortURL.shorten(@link)
-          render template: 'profiles/scrapping'
-      else
-          render template: 'profiles/scrapping'
-      end
+    @name = params[:name]
+    if @name
+      require 'open-uri'
+      require 'shorturl'
+      @doc = Nokogiri::HTML(open('https://twitter.com/' + @name))
+      @name = @doc.css('.ProfileHeaderCard-nameLink').text
+      @bio = @doc.css('.ProfileHeaderCard-bio').text
+      @link = 'https://twitter.com/' + @name
+      @link = ShortURL.shorten(@link)
+      render template: 'profiles/scrapping'
+    else
+      render template: 'profiles/scrapping'
+    end
   end
 
   def new
-      @profile = Profile.new
+    @profile = Profile.new
   end
 
   def create
-      @profile = Profile.new(profile_params)
-      if @profile.save
-          render template: 'profiles/scrapping'
-      end
+    @profile = Profile.new(profile_params)
+    render template: 'profiles/scrapping' if @profile.save
   end
 
   def index
-      @profiles = if params[:term]
-        Profile.where('name ILIKE ?', "%#{params[:term]}%")
-      else
-        @profiles = Profile.all
-      end
+    @profiles = if params[:term]
+                  Profile.where('name ILIKE ?', "%#{params[:term]}%")
+                else
+                  @profiles = Profile.all
+    end
   end
 
   def destroy
     respond_to do |format|
-        @profile.destroy
-        format.html { redirect_to profiles_url, notice: 'Profile was successfully destroyed.' }
+      @profile.destroy
+      format.html { redirect_to profiles_url, notice: 'Profile was successfully destroyed.' }
     end
   end
 
-
   private
-    def set_profile
-        @profile = Profile.find(params[:id])
-    end
 
+  def set_profile
+    @profile = Profile.find(params[:id])
+  end
 
-    def profile_params
-        params.require(:profile).permit(:name, :link, :bio, :term)
-    end
+  def profile_params
+    params.require(:profile).permit(:name, :link, :bio, :term)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
-Rails.application.routes.draw do
+# frozen_string_literal: true
 
+Rails.application.routes.draw do
   resources :profiles
   root 'profiles#scrapping'
   post '/', to: 'profiles#create'
-  delete 'profiles/:id(.:format)', :to => 'profiles#destroy'
-
+  delete 'profiles/:id(.:format)', to: 'profiles#destroy'
 end
